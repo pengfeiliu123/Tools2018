c115e9f36f1dc458952b462b2b5c55b5ca7a9b0d
liupengfei <pengfei.liu@zentertain.net>
2018-02-24 12:02:43 +0800

    add eventbus and networkState Change listener

--- app/src/main/java/com/lpf/tools/feature/networkdemo/NetworkActivity.java
+++ app/src/main/java/com/lpf/tools/feature/networkdemo/NetworkActivity.java
@@ -1,12 +1,20 @@
 package com.lpf.tools.feature.networkdemo;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.lpf.tools.R;
 import com.lpf.tools.network.RequestMethod;
+import com.lpf.utilcode.receiver.NetworkMessage;
+import com.lpf.utilcode.receiver.NetworkStateReceiver;
+import com.lpf.utilcode.util.ToastUtil;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,18 +27,52 @@ public class NetworkActivity extends AppCompatActivity {
     @BindView(R.id.response_text)
     TextView responseText;
 
+    // add network receiver
+    NetworkStateReceiver netWorkStateReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network);
         ButterKnife.bind(this);
 
-        initRequestData(0,10);
+        initRequestData(0, 10);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    protected void onResume() {
+        if (netWorkStateReceiver == null) {
+            netWorkStateReceiver = new NetworkStateReceiver();
+        }
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(netWorkStateReceiver, filter);
+        System.out.println("注册");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(netWorkStateReceiver);
+        System.out.println("注销");
+        super.onPause();
     }
 
     private void initRequestData(int start, int count) {
         //request data.
-        RequestMethod.getInstance().getResponseData(start,count)
+        RequestMethod.getInstance().getResponseData(start, count)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<ResponseEntity>() {
                     Disposable disposable;
@@ -59,5 +101,10 @@ public class NetworkActivity extends AppCompatActivity {
 
     private void onResponseData(ResponseEntity responseEntity) {
         responseText.setText(responseEntity.toString());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(NetworkMessage event) {
+        ToastUtil.showShort(this, event.name + "-->request data");
     }
 }
